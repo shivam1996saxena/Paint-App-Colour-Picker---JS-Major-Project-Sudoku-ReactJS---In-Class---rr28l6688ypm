47b8e952986812e4bc07a2223a6562fe129533c1
shivam1996saxena <110617996+shivam1996saxena@users.noreply.github.com>
2022-12-21 10:31:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -28,14 +28,13 @@ const App = () => {
   const applyColor = (updateSelectionStyle) => {
     updateSelectionStyle(nextBackground)
   }
-
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+      <h5 className="heading">{title}</h5>
 
       <div className="row">
         {colourConfig.map((config, index) => (
-          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
+          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground}/>
         ))}
       </div>
 
